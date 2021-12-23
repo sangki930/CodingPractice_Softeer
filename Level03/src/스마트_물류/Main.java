a9ab7095d1ba8ecfbe5128459d8bd9700a1224f1
sangkipark <sangki931@gmail.com>
2021-12-23 22:20:46 +0900

    commit

---
+++ Level03/src/스마트_물류/Main.java
@@ -0,0 +1,34 @@
+package 스마트_물류;
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main
+{
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]), k =Integer.parseInt(input[1]);
+        input = br.readLine().split(" ");
+        char[] ch=input[0].toCharArray();
+        int answer=0;
+        for(int i=0;i<ch.length;i++){
+            if(ch[i]=='P'){
+                for(int j=-k;j<=k;j++){
+                    if(i+j>=0 && i+j<ch.length){
+                        if(ch[i+j]=='H'){
+                            ch[i]='X';
+                            ch[i+j]='X';
+                            answer++;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(answer);
+        br.close();   
+    }
+}
