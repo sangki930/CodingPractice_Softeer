938817afe9e4649ec1dd9616dc0c233eefc81621
sangkipark <sangki931@gmail.com>
2022-04-30 19:45:44 +0900

    commit

--- Level03/src/좌석관리_미완/Main.java
+++ Level03/src/좌석관리_미완/Main.java
@@ -1,9 +1,10 @@
 package 谅籍包府_固肯;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
 
-// 谅籍包府
 public class Main
 {
     static int[] dx = {-1,-1,1,1}, dy = {-1,1,-1,1};
