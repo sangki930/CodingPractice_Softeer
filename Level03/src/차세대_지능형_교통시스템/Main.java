12368dfa02809310a18ea0ecf4deff0f117a56a9
sangkipark <sangki931@gmail.com>
2022-08-25 23:50:38 +0900

    commit test

--- Level03/src/차세대_지능형_교통시스템/Main.java
+++ Level03/src/차세대_지능형_교통시스템/Main.java
@@ -20,7 +20,7 @@ public class Main
     {0,1,3},{1,0,2},{2,1,3},{3,0,2},
     {0,1},{1,2},{2,3},{3,0},
     {0,3},{1,0},{2,1},{3,2}
-};
+    	};
     public static void main(String args[]) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
