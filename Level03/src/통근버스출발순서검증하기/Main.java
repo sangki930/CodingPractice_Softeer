39dfb1039ba8cf6a800653fbc147d6b413e02d03
sangkipark <sangki931@gmail.com>
2022-10-04 23:23:02 +0900

    commit 통근버스 출발순서 검증하기

---
+++ Level03/src/통근버스출발순서검증하기/Main.java
@@ -0,0 +1,13 @@
+package 통근버스출발순서검증하기;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] t = {};
+		Arrays.stream(t).mapToInt(Integer::parseInt).toArray();
+	}
+
+}
