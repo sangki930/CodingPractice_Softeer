5ed807e1f3238707165e382e8ab7366db3406298
sangki930 <sangki931@gmail.com>
2024-10-13 00:19:57 +0900

    징검다리

--- Level03/src/징검다리/Main.java
+++ Level03/src/징검다리/Main.java
@@ -11,17 +11,17 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int[] dp = new int[n];
-        dp[0] = 1;
-        for(int i=1;i<arr.length;i++){
-            for(int j=i-1;j>=0;j--){
-                if(arr[i]>arr[j]){
-                    dp[i] = Math.max(dp[j]+1,dp[i]);
-                }else{
-                    dp[i] = Math.max(dp[j],dp[i]);
-                }
+        Arrays.fill(dp,1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(arr[i]>arr[j])
+                    dp[i] = Math.max(dp[i],dp[j]+1);
             }
         }
-        System.out.println(dp[n-1]);
+        int answer = 0;
+
+        for(int d  : dp) answer = Math.max(answer,d);
+        System.out.println(answer);
         br.close();
     }
 }
