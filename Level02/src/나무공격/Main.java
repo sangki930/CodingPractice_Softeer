7c12c2d5b39c7b39a1c60cdbcd559eaf82990f8d
sangki930 <sangki931@gmail.com>
2024-10-16 23:58:37 +0900

    나무공격

---
+++ Level02/src/나무공격/Main.java
@@ -0,0 +1,15 @@
+package 나무공격;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+        br.close();
+    }
+}
