dd48efe8f2db8e381cc60e61d641401e5c550c3c
sangki930 <sangki931@gmail.com>
2024-11-07 23:57:42 +0900

    HPEC

---
+++ Level03/src/Pipelined/Main.java
@@ -0,0 +1,19 @@
+package Pipelined;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[n-1] + arr.length - 1);
+
+        br.close();
+    }
+}
