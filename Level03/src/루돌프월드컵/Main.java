20d2d5359eaab3b3be6f68a3b2cb7156fd52d358
sangki930 <sangki931@gmail.com>
2024-11-16 23:46:55 +0900

    루돌프월드컵

--- Level03/src/루돌프월드컵/Main.java
+++ Level03/src/루돌프월드컵/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-
+        int n = Integer.parseInt(br.readLine());
 
         br.close();
     }
